ea3f0e8a019c9bfaa61ee0cf572fee6c0e03edc6
rile037 <99rile@gmail.com>
2023-09-30 11:39:25 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,115 @@
+import java.awt.font.GraphicAttribute;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Racun racun = new Racun(123456789, 25000);
+        Racun.Korisnik korisnik = racun.new Korisnik("Nikola Rilak", "14.08.1999");
+        boolean ispravanPin = false;
+        int brojacPogresnogPina = 1;
+
+        System.out.println("* [TEST] Vas PIN: " + racun.vratiPin());
+
+        while (!ispravanPin){
+            System.out.println("Unesite pin: ");
+            int unetiPin = scanner.nextInt();
+                if (racun.proveriPin(unetiPin)) {
+                    System.out.println("Dobrodosli, " + korisnik.vlasnikRacuna);
+                    prikaziStanje(korisnik);
+                    ispravanPin = true;
+                }
+                else
+                {
+                    if(brojacPogresnogPina < 3) {
+                        System.out.println("Pogresan PIN, pokusajte ponovo");
+                        brojacPogresnogPina++;
+                    }
+                    else{
+                        racun.zamrzniRacun();
+                        System.out.println("Uneli ste pogresan PIN 3 puta.");
+                        System.out.println("Vas racun je BLOKIRAN.");
+                        System.out.println("- Kontaktirajte podrsku ili posetite najblizu filijalu.");
+                        scanner.close();
+                        System.exit(0);
+                    }
+                }
+        }
+            while (true) {
+                prikaziMeni();
+                int izbor = scanner.nextInt();
+
+                switch (izbor) {
+                    case 1 -> podigniNovac(scanner, korisnik, racun);
+                    case 2 -> uplatiNovac(scanner, korisnik);
+                    case 3 -> prikaziStanje(korisnik);
+                    case 0 -> {
+                        zavrsiProgram(scanner);
+                        return;
+                    }
+                    default -> System.out.println("\nNepoznat izbor, pokusajte ponovo.\n");
+                }
+            }
+        }
+
+    private static void prikaziMeni() {
+        System.out.println("1. Podignite");
+        System.out.println("2. Uplatite");
+        System.out.println("3. Stanje racuna\n");
+        System.out.println("0. Izlaz");
+    }
+
+    private static void prikaziStanje(Racun.Korisnik korisnik) {
+        System.out.println("- Stanje vaseg racuna:" + korisnik.getStanje() + "\n");
+    }
+
+    private static void podigniNovac(Scanner scanner, Racun.Korisnik korisnik, Racun racun) {
+        System.out.println("Unesite iznos koji zelite da podignete: ");
+        if (scanner.hasNextDouble()) {
+            double iznos = scanner.nextDouble();
+            if (iznos <= 0 || iznos > racun.getStanje()) {
+                System.out.println("\nNevalidan iznos, pokusajte ponovo.\n");
+            } else {
+                System.out.println("Da li zelite da izvod transakcije?:\n[1 == Da]\n[0 == Ne]");
+                int izbor = scanner.nextInt();
+                korisnik.podigni(iznos);
+                prikaziStanje(korisnik);
+                if(izbor == 1){
+                    korisnik.izvodTransakcije("Isplata",iznos);
+                }
+            }
+        } else {
+            System.out.println("\nNevalidan iznos, pokusajte ponovo.\n");
+            scanner.next();
+        }
+    }
+
+    private static void uplatiNovac(Scanner scanner, Racun.Korisnik korisnik) {
+        System.out.println("\n-Bankomat prima novcanice od: \n- 10, 20, 50, 100, 200, 500, 1000, 2000, 5000\n");
+        System.out.println("Unesite kolicinu novca: ");
+        if (scanner.hasNextDouble()) {
+            double iznos = scanner.nextDouble();
+            if (iznos <= 0) {
+                System.out.println("\nNevalidan iznos, pokusajte ponovo.\n");
+            } else {
+                System.out.println("Da li zelite da izvod transakcije?:\n[1 == Da]\n[0 == Ne]");
+                int izbor = scanner.nextInt();
+                korisnik.uplati(iznos);
+                prikaziStanje(korisnik);
+                if(izbor == 1){
+                    korisnik.izvodTransakcije("Uplata",iznos);
+                }
+            }
+        } else {
+            System.out.println("\nNevalidan iznos, pokusajte ponovo.\n");
+            scanner.next();
+        }
+    }
+
+    private static void zavrsiProgram(Scanner scanner) {
+        System.out.println("\nUzmite Vasu karticu...");
+        scanner.close();
+        System.exit(0);
+    }
+}
---
+++ src/Racun.java
@@ -0,0 +1,122 @@
+import java.util.Random;
+
+public class Racun {
+
+    protected int brojRacuna;
+    private String statusRacuna;
+    protected double stanje;
+    private int pin;
+
+    public Racun(int brojRacuna, double stanje){
+        this.brojRacuna = brojRacuna;
+        this.stanje = stanje;
+        this.statusRacuna = "Aktivan";
+        this.pin = generisiPin();
+    }
+
+    protected int getBrojRacuna(){
+        return this.brojRacuna;
+    }
+    protected void zamrzniRacun(){
+        this.statusRacuna = "Zamrznut";
+    }
+    public String proveriStatusRacuna(){
+        return this.statusRacuna;
+    }
+    public int vratiPin(){
+        return this.pin;
+    }
+    public boolean proveriPin(int unetiPin){
+        return unetiPin == this.pin;
+    }
+    public int generisiPin(){
+        Random random = new Random();
+        return 1000 + random.nextInt(9000);
+    }
+
+    public void promeniPin(int noviPin){
+        this.pin = noviPin;
+    }
+
+    public void setStanje(double stanje){
+        this.stanje = stanje;
+    }
+
+    public double getStanje(){
+        return this.stanje;
+    }
+
+    public class Korisnik extends Racun {
+        String vlasnikRacuna, datumRodjenja;
+        public static int minimum = 500;
+
+        public Korisnik(String vlasnikRacuna, String datumRodjenja) {
+            super(Racun.this.brojRacuna, Racun.this.stanje);
+            this.vlasnikRacuna = vlasnikRacuna;
+            this.datumRodjenja = datumRodjenja;
+        }
+
+        public String maskirajRacun(){
+            String racun = Integer.toString(getBrojRacuna());
+            StringBuilder rezultat = new StringBuilder();
+
+            for(int i = 0; i < racun.length(); i++){
+                  char cifra = racun.charAt(i);
+                  if(i > 4 && i < 9){
+                      rezultat.append("*");
+                  }
+                  else{
+                      rezultat.append(cifra);
+                  }
+              }
+            return rezultat.toString();
+        }
+        protected void izvodTransakcije(String vrstaTransakcije, double iznos){
+            System.out.println("\nVlasnik racuna: " + getVlasnikRacuna());
+            System.out.println("Broj racuna: " + maskirajRacun());
+            System.out.println("--------------------------");
+            System.out.println("Vrsta transakcije: " + vrstaTransakcije);
+            System.out.println("Iznos transakcije: " + iznos);
+            System.out.println("Datum i vreme: \n" );
+        }
+        public String getVlasnikRacuna(){
+            return vlasnikRacuna;
+        }
+
+        public void setVlasnikRacuna(String vlasnikRacuna){
+            this.vlasnikRacuna = vlasnikRacuna;
+        }
+
+        public String getDatumRodjenja(){
+            return datumRodjenja;
+        }
+
+        public void setDatumRodjenja(String datumRodjenja){
+            this.datumRodjenja = datumRodjenja;
+        }
+
+        public void podigni(double iznos){
+            if (iznos <= 0) {
+                System.out.println("\nNevalidan iznos, pokusajte ponovo.\n");
+            } else if (iznos > getStanje()) {
+                System.out.println("\n- Nemate dovoljno sredstava na racunu.\n");
+            } else if(((int) iznos % minimum != 0)){
+                System.out.println("\n- Minimum podizanja po transakciji je: " + minimum + "\n");
+            }
+            else {
+                setStanje(getStanje() - iznos);
+                System.out.println("\n- Uspesno ste podigli " + iznos + " rsd sa racuna.");
+                System.out.println("Stanje racuna: " + getStanje() + "\n");
+            }
+        }
+        public void uplati(double iznos) {
+            if (((int) iznos % 10 != 0) || (((int) iznos < 10))){
+                System.out.println("Bankomat ne prima kovanice.\n");
+            } else {
+                System.out.println("\n- Staro stanje: " + getStanje());
+                setStanje(getStanje() + iznos);
+                System.out.println("- Novo stanje: " + getStanje() + "\n");
+            }
+        }
+    }
+}
